3e2331f2b6d5c455edcadabb7e1fd64025dbbb8a
jandudzik <dudzikjanek@gmail.com>
2020-06-22 15:08:32 +0200

    Scraper for all pages

--- Scraper.R
+++ Scraper.R
@@ -3,33 +3,60 @@ install.packages("xml2")
 library(rvest)
 library(xml2)
 
-#Creating scraper for single page
-site <- xml2::read_html("https://www.otomoto.pl/motocykle-i-quady/chopper--krosowy--motorower--sportowy--turystyczny--typ-cruiser--typ-enduro--typ-naked/?search%5Bfilter_float_engine_capacity%3Afrom%5D=125&search%5Bfilter_float_engine_capacity%3Ato%5D=125&search%5Border%5D=created_at%3Adesc&search%5Bcountry%5D=&page=1")
-motorcycle_model <- site %>%
-  html_nodes(xpath='//*[@id="body-container"]/div[2]/div[1]/div/div[1]/div[5]/article[@data-variation="a"]/div[2]/div[1]/h2/a') %>%
-  html_text()
 
-price <- site %>%
-  html_nodes(xpath='//*[@id="body-container"]/div[2]/div[1]/div/div[1]/div[5]/article[@data-variation="a"]/div[2]/div[2]/div/div/span/span[1]') %>%
-  html_text()
+base_site = "https://www.otomoto.pl/motocykle-i-quady/chopper--krosowy--motorower--sportowy--turystyczny--typ-cruiser--typ-enduro--typ-naked/?search%5Bfilter_float_engine_capacity%3Afrom%5D=125&search%5Bfilter_float_engine_capacity%3Ato%5D=125&search%5Border%5D=created_at%3Adesc&search%5Bcountry%5D=&page=1"
 
-city_name <- site %>%
-  html_nodes(xpath='//*[@id="body-container"]/div[2]/div[1]/div/div[1]/div[5]/article[@data-variation="a"]/div[2]/h4/span[2]') %>%
-  html_text()
+#Scraper for single page
+scrap <- function(site){
+  link <- xml2::read_html(site)
+  
+  motorcycle_model <- link %>%
+    html_nodes(xpath='//*[@id="body-container"]/div[2]/div[1]/div/div[1]/div[5]/article[@data-variation="a"]/div[2]/div[1]/h2/a') %>%
+   html_text()
 
-voivodeship <- site %>%
-  html_nodes(xpath='//*[@id="body-container"]/div[2]/div[1]/div/div[1]/div[5]/article[@data-variation="a"]/div[2]/h4/span[3]') %>%
-  html_text()
+  price <- link %>%
+    html_nodes(xpath='//*[@id="body-container"]/div[2]/div[1]/div/div[1]/div[5]/article[@data-variation="a"]/div[2]/div[2]/div/div/span/span[1]') %>%
+    html_text()
 
-year_of_production <- site %>%
-  html_nodes(xpath='//*[@id="body-container"]/div[2]/div[1]/div/div[1]/div[5]/article[@data-variation="a"]/div[2]/ul/li[1]/span') %>%
-  html_text()
+  city_name <- link %>%
+    html_nodes(xpath='//*[@id="body-container"]/div[2]/div[1]/div/div[1]/div[5]/article[@data-variation="a"]/div[2]/h4/span[2]') %>%
+    html_text()
 
-mileage <- site %>%
-  html_nodes(xpath='//*[@id="body-container"]/div[2]/div[1]/div/div[1]/div[5]/article[@data-variation="a"]/div[2]/ul/li[2]/span') %>%
-  html_text()
+  voivodeship <- link %>%
+    html_nodes(xpath='//*[@id="body-container"]/div[2]/div[1]/div/div[1]/div[5]/article[@data-variation="a"]/div[2]/h4/span[3]') %>%
+    html_text()
 
-data <- cbind(price,city_name,voivodeship,year_of_production,mileage)
+  year_of_production <- link %>%
+    html_nodes(xpath='//*[@id="body-container"]/div[2]/div[1]/div/div[1]/div[5]/article[@data-variation="a"]/div[2]/ul/li[1]/span') %>%
+    html_text()
 
+  mileage <- link %>%
+    html_nodes(xpath='//*[@id="body-container"]/div[2]/div[1]/div/div[1]/div[5]/article[@data-variation="a"]/div[2]/ul/li[2]/span') %>%
+    html_text()
+
+  site_data <- cbind(price,city_name,voivodeship,year_of_production,mileage)
+  return(site_data)
+}
+
+#Scraper for all pages
+get_data <- function(site){
+  link <- xml2::read_html(site)
+  
+  num_of_sites <- link %>%
+    html_nodes(xpath='//*[@id="body-container"]/div[2]/div[2]/ul/li[6]/a/span') %>%
+    html_text()
+  
+  data <- as.data.frame(scrap(site))
+  
+  for(i in 2:num_of_sites){
+    print(paste("Scrapped", i-1, "pages of", num_of_sites))
+    data <- rbind(data, scrap(paste(substr(site, 1, nchar(site)-1), i, sep = "")))
+    Sys.sleep(1)
+  }
+  print("Finished")
+  return(data)
+}
+
+data <- get_data(base_site)
 
 
